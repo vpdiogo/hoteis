712da2418925e5b4428172a73183abc1fd3e1ba1
vpdiogo <vitorpdiogo@gmail.com>
2021-09-22 15:26:15 -0300

    crud implementation and models module

--- hoteis/resources/hotel.py
+++ hoteis/resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -50,14 +51,8 @@ class Hotel(Resource):
     def post(self, hotel_id):
 
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'ciadade': dados['cidade']
-        }
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
 
         hoteis.append(novo_hotel)
         return novo_hotel, 200
@@ -65,7 +60,8 @@ class Hotel(Resource):
     def put(self, hotel_id):
 
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
 
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
@@ -77,4 +73,6 @@ class Hotel(Resource):
         return novo_hotel, 201 # created
 
     def delete(self, hotel_id):
-        ...
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted.'}
