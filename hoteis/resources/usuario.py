abcadb462390622e3e951dfba79c3639e81eacd9
vpdiogo <vitorpdiogo@gmail.com>
2021-10-05 17:25:57 -0300

    login, logout

--- hoteis/resources/usuario.py
+++ hoteis/resources/usuario.py
@@ -1,7 +1,9 @@
+from flask_jwt_extended.utils import get_jwt
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required
 from werkzeug.security import safe_str_cmp
+from blacklist import BLACKLIST
 
 atributos = reqparse.RequestParser()
 atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left in blank")
@@ -15,6 +17,7 @@ class User(Resource):
             return user.json()
         return {'message': 'User not found.'}, 404 # not found
 
+    @jwt_required()
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         
@@ -48,3 +51,11 @@ class UserLogin(Resource):
             token_de_acesso = create_access_token(identity=user.user_id)
             return {'access_token': token_de_acesso}, 200
         return {'message': 'The username or password is incorrect.'}, 401 # Unauthorized
+
+class UserLogout(Resource):
+
+    @jwt_required()
+    def post(self):
+        jwt_id = get_jwt()['jti'] # JWT Token Identifier
+        BLACKLIST.add(jwt_id)
+        return {'message': 'Logged out successfully!'}, 200
