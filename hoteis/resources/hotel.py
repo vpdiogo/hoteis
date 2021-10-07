363109088686c3091661aabcce9b43db40664706
vpdiogo <vitorpdiogo@gmail.com>
2021-10-07 11:57:36 -0300

    filters done

--- hoteis/resources/hotel.py
+++ hoteis/resources/hotel.py
@@ -1,3 +1,4 @@
+from sqlite3.dbapi2 import Cursor
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
@@ -44,26 +45,39 @@ path_params.add_argument('offset', type=float)
 class Hoteis(Resource):
     def get(self):
 
+        connection = sqlite3.connect("base.db")
+        cursor = connection.cursor()
+
         dados = path_params.parse_args()
         dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
         parametros = normalize_path_params(**dados_validos)
 
         if not parametros.get('cidade'):
             consulta = "SELECT * FROM hoteis \
-                        WHERE (estrelas > ? and estrelas < ?) \
-                        and (diaria > ? and diaria < ?) \
+                        WHERE (estrelas >= ? and estrelas <= ?) \
+                        and (diaria >= ? and diaria <= ?) \
                         LIMIT ? OFFSET ?"
             tupla_chaves = tuple([parametros[chave] for chave in parametros])
-            resultado = cursor.excute(consulta, tupla_chaves)
+            resultado = cursor.execute(consulta, tupla_chaves)
         else:
             consulta = "SELECT * FROM hoteis \
-                        WHERE (estrelas > ? and estrelas < ?) \
-                        and (diaria > ? and diaria < ?) \
+                        WHERE (estrelas >= ? and estrelas <= ?) \
+                        and (diaria >= ? and diaria <= ?) \
                         and cidade = ? LIMIT ? OFFSET ?"
             tupla_chaves = tuple([parametros[chave] for chave in parametros])
-            resultado = cursor.excute(consulta, tupla_chaves)
+            resultado = cursor.execute(consulta, tupla_chaves)
 
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} # SELECT * FROM hoteis
+        hoteis = []
+        for linha in resultado:
+            hoteis.append({
+            'hotel_id': linha[0],
+            'nome': linha[1],
+            'estrelas': linha[2] ,
+            'diaria': linha[3],
+            'cidade': linha[4]
+            })
+
+        return {'hoteis': hoteis} # SELECT * FROM hoteis
 
 class Hotel(Resource):
     atributos = reqparse.RequestParser()
