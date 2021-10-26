7d8f80bd5b4c840ffb0c8f17f17ea0e10353502c
vpdiogo <vitorpdiogo@gmail.com>
2021-10-26 10:14:59 -0300

    create config json file

---
+++ hoteis/config_json.py
@@ -0,0 +1,11 @@
+import json
+
+with open('credentials.json') as json_file:
+    config = json.load(json_file)
+
+USER = config.get("USER")
+PASSWORD = config.get("PASSWORD")
+DATABASE = config.get("DATABASE")
+JWT_SECRET_KEY = config.get("JWT_SECRET_KEY")
+PORT = config.get("PORT")
+HOST = config.get("HOST")
